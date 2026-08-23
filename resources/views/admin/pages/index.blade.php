4555fcd57818d40961410aa6826a7ffb4f53f15d
joshualion <joshuaekep87@gmail.com>
2026-08-23 09:15:55 +0100

    patch

--- resources/views/admin/pages/index.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- resources/views/admin/dashboard.blade.php --}}
-<x-admin-layout title="Frontend">
-    <div class="bg-white p-6 rounded shadow">
-        <h2 class="text-xl font-semibold">Pages</h2>
-        <p class="mt-2 text-gray-600">Here’s a quick overview of today’s activity.</p>
-    </div>
-</x-admin-layout>
-
-{{-- resources/views/components/frontend-layout.blade.php --}}
